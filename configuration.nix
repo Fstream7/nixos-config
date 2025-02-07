638052b2700761fa848ea11eddd4fbc4fc9a4318
Fstream7 <vania.tkachuk@gmail.com>
2025-02-07 11:20:13 +0200

    open dns and dhcp ports for wifi hotspot

--- configuration.nix
+++ configuration.nix
@@ -146,6 +146,10 @@
   # Or disable the firewall altogether.
   # networking.firewall.enable = false;
 
+  # Open DHCP and DNS ports for hotspot
+  networking.firewall.allowedUDPPorts = [ 53 67 68 ];
+  networking.firewall.allowedTCPPorts = [ 53 ];
+
   # This value determines the NixOS release from which the default
   # settings for stateful data, like file locations and database versions
   # on your system were taken. It‘s perfectly fine and recommended to leave
