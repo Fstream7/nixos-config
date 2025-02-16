c897efb6e48b6c127ef5cee56119d3ae8d1f86e5
Fstream7 <vania.tkachuk@gmail.com>
2025-02-16 19:07:03 +0200

    enable opengl, nvidia prime, gamemode, mangohud for gaming

--- nvidia.nix
+++ nvidia.nix
@@ -1,6 +1,12 @@
 { config, lib, pkgs, ... }:
 {
 
+  # Enable OpenGL
+  hardware.graphics = {
+    enable = true;
+    enable32Bit = true;
+  };
+
   # Load nvidia driver for Xorg and Wayland
   services.xserver.videoDrivers = ["nvidia"];
 
@@ -29,10 +35,23 @@
     open = false;
 
     # Enable the Nvidia settings menu,
-	# accessible via `nvidia-settings`.
+	  # accessible via `nvidia-settings`.
     nvidiaSettings = true;
 
     # Optionally, you may need to select the appropriate driver version for your specific GPU.
     package = config.boot.kernelPackages.nvidiaPackages.stable;
   };
+
+	hardware.nvidia.prime = {
+		offload = {
+			enable = true;
+			enableOffloadCmd = true;
+		};
+    # integrated
+		intelBusId = "PCI:0:2:0";
+    # dedicated
+		nvidiaBusId = "PCI:01:0:0";
+	};
+
+
 }
--- software/steam.nix
+++ software/steam.nix
@@ -6,5 +6,12 @@
     remotePlay.openFirewall = true; # Open ports in the firewall for Steam Remote Play
     dedicatedServer.openFirewall = true; # Open ports in the firewall for Source Dedicated Server
     localNetworkGameTransfers.openFirewall = true; # Open ports in the firewall for Steam Local Network Game Transfers
+    gamescopeSession.enable = true; # enable GameScope Session
   };
+
+  environment.systemPackages = with pkgs; [
+    mangohud # monitoring system performance
+  ];
+  programs.gamemode.enable = true;
+
 }
