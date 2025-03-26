85369d35a23a0801002ad65aa57c2573164d5da9
Fstream7 <vania.tkachuk@gmail.com>
2025-03-26 17:19:46 +0200

    add mvp

--- configuration.nix
+++ configuration.nix
@@ -110,6 +110,7 @@
     #  vim # Do not forget to add an editor to edit configuration.nix! The Nano editor is also installed by default.
     #  wget
     neovim
+    mpv
     atop
     lm_sensors
     ethtool
