4864353ec988f20375dcc5283273550f73797b7d
Fstream7 <vania.tkachuk@gmail.com>
2025-03-31 21:56:12 +0300

    add env selector for vscodium

--- software/vscodium.nix
+++ software/vscodium.nix
@@ -11,6 +11,7 @@ environment.systemPackages = with pkgs; [
       ms-python.python
       ms-python.flake8
       ms-python.black-formatter
+      arrterian.nix-env-selector
       hashicorp.terraform
     ];
   })
