d4f882fc1f2b3324bbb8c5079aa6d08cd5e37052
Fstream7 <vania.tkachuk@gmail.com>
2026-05-22 17:39:03 +0300

    upgrade, fix enpass

--- configuration.nix
+++ configuration.nix
@@ -157,6 +157,21 @@
   ];
 
 
+# fix for enpass
+  nixpkgs.overlays = [
+    (final: prev: {
+      enpass = prev.enpass.overrideAttrs (old: rec {
+        version = "6.11.13.1957";
+
+        src = prev.fetchurl {
+          url = "https://apt.enpass.io/pool/main/e/enpass/enpass_${version}_amd64.deb";
+          sha256 = "2d8c90643851591aff41057b380a7e87bb839bf5c5aa0ca1456144e9996c902a";
+        };
+      });
+    })
+  ];
+
+
   # enable docker
   virtualisation.docker.enable = true;
 
